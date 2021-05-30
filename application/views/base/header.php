a9b0b39aabcb1908bfe3077e1cdd2e70ba12870d
DESKTOP-9NGASCB\DELL <honghue2911999@gmail.com>
2021-05-30 16:17:47 +0700

    update view header

--- application/views/base/header.php
+++ application/views/base/header.php
@@ -1,6 +1,10 @@
+<?php $viewUser=false;?>
+
+
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 
 button.dropbtn a{
@@ -35,17 +39,95 @@ button.dropbtn a{
   text-decoration: none;
   display: block;
 }
+.inline{
+  display: flex;
+  justify-content:space-between;
+align-items:center;
+height:100px;
 
+}
+.search-input{
+  height:40px;
+  font-size:18px;
+  width:400px;
+  padding-left:10px;
+  border-top:1px solid rgb(118, 118, 118);
+  border-left:1px solid rgb(118, 118, 118);
+  border-bottom:1px solid rgb(118, 118, 118);
+  border-right:unset;
+  padding-bottom:1px !important;
+}
+.btn-search{
+  color:#ffff;
+  height:40px;
+  width:100px;
+  background-color: #04AA6D;
+  border:1px solid #04AA6D ;
+  border-radius:0px 8px 8px 0px;
+}
 .dropdown-content a:hover {background-color: #ddd;}
 
 .dropdown:hover .dropdown-content {display: block;}
 
 .dropdown:hover .dropbtn {background-color: #3e8e41;}
-</style>
+.tooltip {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+}
 
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: #c3f7e4;
+  color:black;
+  font-size:18px;
+  border-radius: 6px;
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+  top: 120%;
+ right:5px;
+ 
+ 
+}
+ul {
+list-style: none;
+}
+
+</style>
+<script>
+var isViewUser=false;
+visible=()=>{
+  if(!isViewUser)
+  {document.getElementById("tooltip").style.visibility = "visible";
+isViewUser=true;
+}
+else{
+  document.getElementById("tooltip").style.visibility = "hidden";
+isViewUser=false;
+}
+
+}
+
+
+</script>
 </head>
 <body>
-<p>This is header</p>
+<div style="margin-left:15px">
+<div class="inline">
+<div><img src="" alt="logo"/></div>
+<div><input class="search-input" placeholder="Áo sơ mi"/><button class="btn-search">Search</button></div>
+<div style="margin-right:15px"><i class="fa fa-cart-plus" style="font-size:35px;margin-right:15px"></i><i class="fa fa-user tooltip" style="font-size:35px" onclick="visible()">
+
+<div class="tooltiptext" id="tooltip" style="visibility:hidden"><ul>
+<li style="margin-bottom:10px">username</li>
+<li  style="cursor:pointer">logout</li>
+</ul></div>
+
+</i> </div>
+
+</div>
 <nav>
 <div class="dropdown"><button class="dropbtn" ><a href="?url=home/view">Home</a></button></div>
 <div class="dropdown">
@@ -69,5 +151,6 @@ button.dropbtn a{
 
 </div>
 </nav>
+</div>
 </body>
 </html>
