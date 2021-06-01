4d84d7514cfffcc945730a5c9e621403c039f274
DESKTOP-9NGASCB\DELL <honghue2911999@gmail.com>
2021-06-01 16:02:01 +0700

    update view category

--- application/views/category/view.php
+++ application/views/category/view.php
@@ -1,3 +1,63 @@
 <?php
-print_r($productList);
+// print_r($products);
 ?>
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+  .product{
+    margin-top:20px;
+      display:flex;
+      flex-direction:column;
+      align-items:center;
+        height:250px;
+        width: 300px;
+  }
+  .inline-row {
+      display: flex;
+      flex-direction:row;
+      justify-content:start;
+      flex-wrap:wrap;
+      
+  }
+.item{
+    height:300;
+    
+     width:200; 
+     background: rgb(245, 245, 245)
+}
+
+  </style>
+
+</head>
+<body>
+<div class="content-product">
+<div class="inline-row">
+<?php
+
+ foreach($products as $item ){
+     ?>
+     <div class="product">
+     <div class="item" >
+     <img src="<?php echo PATH_URL_IMG_PRODUCT.$item['Product']['Image1']?>"  alt="product" height=200 width=200/>
+     <p> <?php echo $item['Product']['Name'];?>   </p>
+     </div>
+     </div>
+<?php
+  
+ }
+
+?>
+
+
+</div>
+
+
+</div>
+
+
+
+</body>
+
+</html>
--- config/config.php
+++ config/config.php
@@ -5,12 +5,13 @@
 define('DEVELOPMENT_ENVIRONMENT', true);
 
 define('DB_NAME', 'ecommerce');
-define('DB_USER', 'hangtt');
-define('DB_PASSWORD', '1');
-// define('DB_USER', 'duonghue1');
-// define('DB_PASSWORD', 'duonghue');
+//define('DB_USER', 'hangtt');
+//define('DB_PASSWORD', '1');
+ define('DB_USER', 'duonghue1');
+ define('DB_PASSWORD', 'duonghue');
 define('DB_HOST', 'localhost');
 define('PATH_URL', 'http://localhost/web-programming-e-commerce');
 define('PATH_URL_IMG', PATH_URL.'/public/images/');
 define('PATH_URL_IMG_HOME', PATH_URL.'/public/images/home/');
+define('PATH_URL_IMG_LOGO', PATH_URL.'/public/images/logo/');
 define('PATH_URL_IMG_PRODUCT', PATH_URL.'/public/images/product/');
