1d2f2f533c4b3b3cf6def35e4cb52d3a11851357
DESKTOP-9NGASCB\DELL <honghue2911999@gmail.com>
2021-06-06 16:01:48 +0700

    custom login form

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -12,25 +12,29 @@
                 <div class="login-panel panel panel-default">
                     
                     <div class="panel-body">
-                        <div class="inline">
-                        <h3 class="panel-title">Login</h3>
-                        </div>
+                      
                         <div class="form-login">
                         <form method="post" action="/user/login" class="form-signin" role="form">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="username" type="text" autofocus>
+                            <div class="loginForm">
+                            <div style="text-align:center; font-weight:bold;font-size:30px">Welcome to 3H!</div>
+                             
+                            <div class="form-group">
+                            <p class="label">Username</p> 
+                                    <input class="form-control" placeholder="username" name="username" type="text" autofocus>
                                 </div>
+                             
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                <p class="label">Password</p>
+                                    <input class="form-control" placeholder="Password" name="password" type="password" value=""/>
                                 </div>
-                                <div class="checkbox">
+                                <div style="width:412px">
                                     <label>
                                         <input name="remember" type="checkbox" value="Remember Me">Remember Me
                                     </label>
                                 </div>
-                                <button class="btn btn-lg btn-primary btn-block" type="submit">Đăng Nhập</button>
-                            </fieldset>
+                                <button class="btn-primary" type="submit">Đăng Nhập</button>
+                            </div>
+                                </div>
                         </form>
                         </div>
                     </div>
